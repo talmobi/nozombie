6173bb606949c000a28e3cd4a426d30dd72c4829
Christian Talmo <talmo.christian@gmail.com>
2019-06-18 12:02:00 +0300

    refactor api.clean pids list reset to use api.reset

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function nozombie () {
     parallelLimit( tasks, 3, function ( err, results ) {
       // clean _pids list if everything went OK
       if ( !err && results.length === tasks.length ) {
-        _pids = []
+        _api.reset()
       }
 
       if ( done ) {
