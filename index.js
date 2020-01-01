2a92a0f9a557c6f85156252bcdb0264ad709374f
Christian Talmo <talmo.christian@gmail.com>
2020-01-01 04:31:53 +0200

    feat: periodically remove exited processes from being tracked

--- index.js
+++ index.js
@@ -12,14 +12,68 @@ function debug () {
 var _nozombies = []
 
 process.on( 'exit', function () {
+  clearTimeout( _tick_timeout )
   _nozombies.forEach( function ( nz ) {
     nz.kill()
   } )
 } )
 
-// TODO add min ttl for added processes
+const TICK_INTERVAL = 1000 * 90
+let _tick_timeout = undefined
 
-function nozombie () {
+// remove non-running pid's from pids lists over time
+function tick () {
+  debug( 'ticking' )
+  const map = {} // map of pids
+
+  _nozombies.forEach( function ( nz ) {
+    nz.list().forEach( function ( pid ) {
+      map[ pid ] = pid
+    } )
+  } )
+
+  const keep = {}
+
+  psList()
+  .then( function ( list ) {
+    for ( let i = 0; i < list.length; i++ ) {
+      const item = list[ i ]
+      const pid = item.pid
+      keep[ pid ] = true
+    }
+
+    next()
+  } )
+  .catch( function ( err ) {
+    debug( 'tick psList error: ' + err )
+    next()
+  } )
+
+  function next () {
+    const pids = Object.keys( map )
+
+    pids.forEach( function ( pid ) {
+      if ( keep[ pid ] ) {
+        // do nothing, keep it in
+      } else {
+        // process does not exist anymore -> stop tracking it
+        // (remove from list)
+        _nozombies.forEach( function ( nz ) {
+          nz._forget( pid )
+        } )
+      }
+    } )
+
+    clearTimeout( _tick_timeout )
+    _tick_timeout = undefined
+
+    if ( pids.length > 0 ) {
+      _tick_timeout = setTimeout( tick, TICK_INTERVAL )
+    }
+  }
+}
+
+function nozombie ( opts ) {
   const _api = {}
 
   _nozombies.push( _api )
@@ -31,6 +85,12 @@ function nozombie () {
   let _pids = []
 
   _api.push = _api.add = function push ( pid ) {
+    // start ticking
+    if ( _tick_timeout === undefined ) {
+      clearTimeout( _tick_timeout )
+      _tick_timeout = setTimeout( tick, TICK_INTERVAL )
+    }
+
     if ( typeof pid === 'object' ) {
       if ( pid.pid ) pid = pid.pid
     }
@@ -143,6 +203,15 @@ function nozombie () {
           function next () {
             // debug( 'inside next: ' + !!ok )
 
+            for ( let i = 0; i < _nozombies.length; i++ ) {
+              const nz = _nozombies[ i ]
+              if ( nz._size ) {
+                clearTimeout( _tick_timeout )
+                _tick_timeout = undefined
+                break
+              }
+            }
+
             if ( ok ) {
               if ( done ) {
                 done( err, results )
@@ -175,6 +244,21 @@ function nozombie () {
 
   _api.reset = function reset () {
     return _pids = []
+  }
+
+  _api._forget = function _forget ( pid ) {
+    // remove pid's that aren't running anymore
+    // this will help prevent accidentally killing future
+    // processes that have been given the same pid (as they are
+    // semi-randomly allocated by the OS)
+    var i = _pids.indexOf( pid )
+    if ( i >= 0 ) {
+      _pids.splice( i, 1 )
+    }
+  }
+
+  _api._size = function _size () {
+    return _pids.length
   }
 
   return _api
