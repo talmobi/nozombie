e68ff16b1583554da6572c6873773fcd9581ddf4
Christian Talmo <talmo.christian@gmail.com>
2020-02-22 16:49:57 +0200

    test: change api.clean tests with new feature of only cleaning ( no killing )

--- test/index.js
+++ test/index.js
@@ -361,197 +361,23 @@ test( 'cleaning zombies', function ( t ) {
 
   process.nextTick( function () {
     setTimeout( function () {
-      nz.clean( function ( err, killedPids ) {
+      nz.clean( function ( err, size ) {
         if ( err ) {
           t.error( err )
         }
 
-        t.equal( killedPids.length, 9, 'killed length OK!' )
+        t.equal( size, 6, 'size OK! (none killed yet)' )
         t.equal( finishCounter, 3, 'only first three spawns finished OK!' )
 
-        setTimeout( function () {
-          t.equal( exitCounter, 9, 'all spawns exited OK!' )
-          t.end()
-        }, 500 )
-      } )
-    }, 500 )
-  } )
-} )
-
-test( 'no error cleaning dead zombies', function ( t ) {
-  const nz = nozombie()
-
-  // spawns exited
-  let exitCounter = 0
-
-  // spawns who exited naturally at the end
-  let finishCounter = 0
-
-  function spawnChild ( ms ) {
-    const spawn = childProcess.spawn( 'node', [ 'test/mocks/spawn.js', ms ] )
-
-    nz.add( spawn.pid )
-
-    spawn.stdout.on( 'data', function ( data ) {
-      str = String( data )
-
-      if ( str.indexOf( 'done' ) >= 0 ) {
-        finishCounter++
-      }
-    } )
-
-    spawn.on( 'exit', function () {
-      exitCounter++
-    } )
-  }
-
-  spawnChild( 100 )
-  spawnChild( 200 )
-  spawnChild( 300 )
-  spawnChild( 4000 )
-  spawnChild( 5000 )
-  spawnChild( 6000 )
-  spawnChild( 7000 )
-  spawnChild( 8000 )
-  spawnChild( 9000 )
-
-  process.nextTick( function () {
-    setTimeout( function () {
-      nz.clean( function ( err, killedPids ) {
-        if ( err ) {
-          t.error( err )
-        }
-
-        t.equal( killedPids.length, 9, 'killed length OK!' )
-        t.equal( finishCounter, 3, 'only first three spawns finished OK!' )
-
-        setTimeout( function () {
-          t.equal( exitCounter, 9, 'all spawns exited OK!' )
-          // attempt to clean the same processes again
-          nz.clean( function ( err, killedPids ) {
-            if ( err ) {
-              t.error( err )
-            }
-
-            // result is 0 now because the cleanup cleared
-            // the pids list
-            t.equal( killedPids.length, 0, 'killed length OK!' )
-
-            // finish counter still the same
-            t.equal( finishCounter, 3, 'only first three spawns finished OK!' )
-
-            // exit counter still the same
+        nz.kill( function ( err, killedPids ) {
+            t.equal( killedPids.length, 6, 'killed counter OK!' )
             t.equal( exitCounter, 9, 'all spawns exited OK!' )
 
-            t.end()
-          } )
-
-        }, 500 )
-      } )
-    }, 500 )
-  } )
-} )
-
-test( 'no error cleaning non-existing processes', async function ( t ) {
-  const nz = nozombie()
-
-  // spawns exited
-  let exitCounter = 0
-
-  // spawns who exited naturally at the end
-  let finishCounter = 0
-
-  async function lookup ( pid ) {
-    return new Promise( function ( resolve, reject ) {
-      usage.lookup( pid, function ( err, result ) {
-        if ( err ) return reject( err )
-        resolve( result )
-      } )
-    } )
-  }
-
-  async function getFreePid () {
-    let pid = 32768
-
-    return new Promise( async function ( resolve, reject ) {
-      let done = false
-      while ( !done ) {
-        try {
-          await lookup( pid-- ) // ignore result
-        } catch ( err ) {
-          // pid not found, we can use this "free" pid
-          done = true
-        }
-      }
-
-      resolve( pid )
-    } )
-  }
-
-  nz.add( await getFreePid() )
-
-  function spawnChild ( ms ) {
-    const spawn = childProcess.spawn( 'node', [ 'test/mocks/spawn.js', ms ] )
-
-    nz.add( spawn.pid )
-
-    spawn.stdout.on( 'data', function ( data ) {
-      str = String( data )
-
-      if ( str.indexOf( 'done' ) >= 0 ) {
-        finishCounter++
-      }
-    } )
-
-    spawn.on( 'exit', function () {
-      exitCounter++
-    } )
-  }
-
-  spawnChild( 100 )
-  spawnChild( 200 )
-  spawnChild( 300 )
-  spawnChild( 4000 )
-  spawnChild( 5000 )
-  spawnChild( 6000 )
-  spawnChild( 7000 )
-  spawnChild( 8000 )
-  spawnChild( 9000 )
-
-  process.nextTick( function () {
-    setTimeout( function () {
-      nz.clean( function ( err, killedPids ) {
-        if ( err ) {
-          t.error( err )
-        }
-
-        // one extra result for the non-existing pid
-        t.equal( killedPids.length, 10, 'killed length OK!' )
-        t.equal( finishCounter, 3, 'only first three spawns finished OK!' )
-
-        setTimeout( function () {
-          t.equal( exitCounter, 9, 'all spawns exited OK!' )
-          // attempt to clean the same processes again
-          nz.clean( function ( err, killedPids ) {
-            if ( err ) {
-              t.error( err )
-            }
-
-            // result is 0 now because the cleanup cleared
-            // the pids list
-            t.equal( killedPids.length, 0, 'killed length OK!' )
-
-            // finish counter still the same
-            t.equal( finishCounter, 3, 'only first three spawns finished OK!' )
-
-
-            // exit counter still the same
-            t.equal( exitCounter, 9, 'all spawns exited OK!' )
-
-            t.end()
-          } )
-
-        }, 500 )
+            nz.clean( function ( err, size ) {
+              t.equal( size, 0, 'size OK! (everything is dead)' )
+              t.end()
+            } )
+        } )
       } )
     }, 500 )
   } )
